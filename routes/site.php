d0e087c0469c8d16d1c15bc7e3a5e0e7ed1dd813
quangdv <quangdovinh@bizfly.vn>
2023-12-15 14:39:40 +0700

    name controller

--- routes/site.php
+++ routes/site.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'SitehomeController@index')->name('home.index');
-Route::get('_{code}', 'SitehomeController@account')->name('home.account');
-Route::get('logout', 'SitehomeController@logout')->name('home.logout');
+Route::get('/', 'SiteHomeController@index')->name('home.index');
+Route::get('_{code}', 'SiteHomeController@account')->name('home.account');
+Route::get('logout', 'SiteHomeController@logout')->name('home.logout');
 
-Route::post('join', 'SitehomeController@join')->name('home.join');
-Route::post('submit', 'SitehomeController@submit')->name('home.submit');
+Route::post('join', 'SiteHomeController@join')->name('home.join');
+Route::post('submit', 'SiteHomeController@submit')->name('home.submit');
